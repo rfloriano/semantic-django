c00ee1e4a8c297e05a762841353edd9f376bb166
Rafael Floriano da Silva <rflorianobr@gmail.com>
2012-10-19 01:46:42 -0300

    No need to assert response.status_code is 200, the assertContains already does this

--- example_project/example_app/tests/functional/test_base_programa.py
+++ example_project/example_app/tests/functional/test_base_programa.py
@@ -13,7 +13,6 @@ class TestBasePrograma(SemanticTestCase):
 
     def test_if_can_edit_a_baseprograma_objects_in_admin(self):
         response = self.client.get('/admin/example_app/baseprograma/http_3A_2F_2Fsemantica.globo.com_2Fbase_2FPrograma_5FRock_5Fin_5FRio/')
-        self.assertEqual(response.status_code, 200)
         self.assertContains(response, '<input id="id_faz_parte_do_canal" type="text" name="faz_parte_do_canal" value="http://semantica.globo.com/base/Canal_Multishow" maxlength="200" />')
         self.assertContains(response, '<input id="id_foto_perfil" type="text" name="foto_perfil" maxlength="200" />')
         self.assertContains(response, '<input id="id_tem_edicao_do_programa" type="text" name="tem_edicao_do_programa" maxlength="200" />')
@@ -23,7 +22,6 @@ class TestBasePrograma(SemanticTestCase):
 
     def test_if_can_add_baseprograma_objects_in_admin(self):
         response = self.client.get('/admin/example_app/baseprograma/add/')
-        self.assertEqual(response.status_code, 200)
         self.assertContains(response, '<input id="id_faz_parte_do_canal" type="text" name="faz_parte_do_canal" maxlength="200" />')
         self.assertContains(response, '<input id="id_foto_perfil" type="text" name="foto_perfil" maxlength="200" />')
         self.assertContains(response, '<input id="id_tem_edicao_do_programa" type="text" name="tem_edicao_do_programa" maxlength="200" />')
@@ -34,5 +32,4 @@ class TestBasePrograma(SemanticTestCase):
     def test_field_label_has_label_label(self):
         # regression test related to bug introduced in b1bb72e6f386bdf25e35d69d01ef1da417e4b20e
         response = self.client.get('/admin/example_app/baseprograma/add/')
-        self.assertEqual(response.status_code, 200)
         self.assertContains(response, '<label for="id_uri" class="required">Uri:</label>')
